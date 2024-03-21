8ce66079e86b6f15289f7137099ceb0fc350bf92
Adam B <brunk01@gmail.com>
2024-03-21 17:14:59 -0400

    Create doctypes.sql

---
+++ doctypes.sql
@@ -0,0 +1,7 @@
+INSERT INTO doctypes (name) VALUES ('Medical School Diploma');
+INSERT INTO doctypes (name) VALUES ('Medical License');
+INSERT INTO doctypes (name) VALUES ('Board Certification');
+INSERT INTO doctypes (name) VALUES ('Residency Completion Certificate');
+INSERT INTO doctypes (name) VALUES ('Fellowship Completion Certificate');
+INSERT INTO doctypes (name) VALUES ('Malpractice Insurance');
+INSERT INTO doctypes (name) VALUES ('Continuing Medical Education Certificates');
